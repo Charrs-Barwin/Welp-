1c7c0a8ca3a2c4fe350dac38c2800c9acbe9508b
Charrs-Barwin <mikehollander84@gmail.com>
2022-06-20 17:11:07 -0400

    setup debugging

---
+++ frontend/welp.jsx
@@ -0,0 +1,9 @@
+// frontend/welp.jsx
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>Welcome to Welp!</h1>, root);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
 const path = require('path');
 
 module.exports = {
-  entry: './frontend/welp.jsx',
-  output: './app/assets/javascripts',
-//   {
-//     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-//     filename: 'bundle.js'
-//   },
-  module: {
-    rules: [
-      {
-        test: [/\.jsx?$/],
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['env', 'react']
-        }
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '*'],
-  }
+    context: __dirname,
+    entry: "./frontend/welp.jsx",
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: "./bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [".js", ".jsx", "*"]
+    }
 };
