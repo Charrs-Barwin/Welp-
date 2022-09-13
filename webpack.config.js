1b6340b58d0c15fe3acf227f31977249dcbb307a
Charrs-Barwin <mikehollander84@gmail.com>
2022-09-13 17:44:54 -0400

    install file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
                         presets: ['@babel/env', '@babel/react']
                     }
                 },
+            },
+            {
+              test: /\.(gif|svg|jpg|png)$/,
+              loader: "file-loader",
             }
         ]
     },
