23523016621fd4714c88cee65d523e6940bcbaa6
Charrs-Barwin <mikehollander84@gmail.com>
2022-11-01 20:47:03 -0400

    setup generated seeds with photos for 20 businesses

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,46 +16,99 @@ ActiveRecord::Base.connection.tables.each do |t|
 end
 
 require 'faker'
+Faker::UniqueGenerator.clear
 # Faker::Name.name      #=> "Christophe Bartell"
 # Faker::Internet.email #=> "kirsten.greenholt@corkeryfisher.info"
 
 require 'open-uri'
-b1 = open('https://welp--seeds.s3.amazonaws.com/b1.png')
-b2 = open('https://welp--seeds.s3.amazonaws.com/b2.jpg')
-b3 = open('https://welp--seeds.s3.amazonaws.com/b3.jpg')
-# test = open('https://images.pexels.com/photos/1267320/pexels-photo-1267320.jpeg')
+# b1 = open('https://welp--seeds.s3.amazonaws.com/b1.png')
+# b2 = open('https://welp--seeds.s3.amazonaws.com/b2.jpg')
+# b3 = open('https://welp--seeds.s3.amazonaws.com/b3.jpg')
 
-# attach to user
-# bigburger.photo.attach(io: b1, filename: 'b1.png')
-# midburger.photo.attach(io: b2, filename: 'b2.jpg')
-# littleburger.photo.attach(io: b3, filename: 'b3.jpg')
+
+stockPhotoUrls = [
+    'https://images.pexels.com/photos/2331536/pexels-photo-2331536.jpeg',
+    'https://images.pexels.com/photos/194913/pexels-photo-194913.jpeg',
+    'https://images.pexels.com/photos/5713766/pexels-photo-5713766.jpeg',
+    'https://images.pexels.com/photos/343871/pexels-photo-343871.jpeg',
+    'https://images.pexels.com/photos/983587/pexels-photo-983587.jpeg',
+    'https://images.pexels.com/photos/916416/pexels-photo-916416.jpeg',
+    'https://images.pexels.com/photos/8630151/pexels-photo-8630151.jpeg',
+    'https://images.pexels.com/photos/343870/pexels-photo-343870.jpeg',
+    'https://images.pexels.com/photos/2919579/pexels-photo-2919579.jpeg',
+    'https://images.pexels.com/photos/2781537/pexels-photo-2781537.jpeg',
+    'https://images.pexels.com/photos/6270541/pexels-photo-6270541.jpeg',
+    'https://images.pexels.com/photos/1694865/pexels-photo-1694865.jpeg',
+    'https://images.pexels.com/photos/4062274/pexels-photo-4062274.jpeg',
+    'https://images.pexels.com/photos/836850/pexels-photo-836850.jpeg',
+    'https://images.pexels.com/photos/4059426/pexels-photo-4059426.jpeg',
+    'https://images.pexels.com/photos/2781540/pexels-photo-2781540.jpeg',
+    'https://images.pexels.com/photos/9919799/pexels-photo-9919799.jpeg',
+    'https://images.pexels.com/photos/3215513/pexels-photo-3215513.jpeg',
+    'https://images.pexels.com/photos/2317542/pexels-photo-2317542.jpeg',
+    'https://images.pexels.com/photos/52576/pexels-photo-52576.jpeg',
+    'https://images.pexels.com/photos/4669246/pexels-photo-4669246.jpeg',
+    'https://images.pexels.com/photos/6240974/pexels-photo-6240974.jpeg',
+    'https://images.pexels.com/photos/8697543/pexels-photo-8697543.jpeg',
+    'https://images.pexels.com/photos/7627414/pexels-photo-7627414.jpeg',
+    'https://images.pexels.com/photos/7627422/pexels-photo-7627422.jpeg',
+    'https://images.pexels.com/photos/4669251/pexels-photo-4669251.jpeg',
+    'https://images.pexels.com/photos/5192027/pexels-photo-5192027.jpeg',
+    'https://images.pexels.com/photos/6240876/pexels-photo-6240876.jpeg',
+    'https://images.pexels.com/photos/6373441/pexels-photo-6373441.jpeg',
+    'https://images.pexels.com/photos/4586810/pexels-photo-4586810.jpeg',
+    'https://images.pexels.com/photos/4062272/pexels-photo-4062272.jpeg',
+    'https://images.pexels.com/photos/6270651/pexels-photo-6270651.jpeg',
+    'https://images.pexels.com/photos/6270663/pexels-photo-6270663.jpeg',
+    'https://images.pexels.com/photos/5616132/pexels-photo-5616132.jpeg',
+    'https://images.pexels.com/photos/5616129/pexels-photo-5616129.jpeg',
+    'https://images.pexels.com/photos/6373255/pexels-photo-6373255.jpeg',
+    'https://images.pexels.com/photos/6382782/pexels-photo-6382782.jpeg',
+    'https://images.pexels.com/photos/6382822/pexels-photo-6382822.jpeg',
+    'https://images.pexels.com/photos/11213692/pexels-photo-11213692.jpeg',
+    'https://images.pexels.com/photos/4809147/pexels-photo-4809147.jpeg',
+    'https://images.pexels.com/photos/4566718/pexels-photo-4566718.jpeg',
+    'https://images.pexels.com/photos/5192030/pexels-photo-5192030.jpeg',
+    'https://images.pexels.com/photos/9311856/pexels-photo-9311856.jpeg',
+    'https://images.pexels.com/photos/8743917/pexels-photo-8743917.jpeg',
+    'https://images.pexels.com/photos/9329433/pexels-photo-9329433.jpeg',
+    'https://images.pexels.com/photos/6241091/pexels-photo-6241091.jpeg',
+    'https://images.pexels.com/photos/6241118/pexels-photo-6241118.jpeg',
+    'https://images.pexels.com/photos/6240997/pexels-photo-6240997.jpeg',
+    'https://images.pexels.com/photos/10132214/pexels-photo-10132214.jpeg',
+    'https://images.pexels.com/photos/5616153/pexels-photo-5616153.jpeg',
+    'https://images.pexels.com/photos/6383072/pexels-photo-6383072.jpeg',
+    'https://images.pexels.com/photos/6383081/pexels-photo-6383081.jpeg',
+    'https://images.pexels.com/photos/6383082/pexels-photo-6383082.jpeg',
+    'https://images.pexels.com/photos/7627441/pexels-photo-7627441.jpeg',
+    'https://images.pexels.com/photos/7214136/pexels-photo-7214136.jpeg',
+    'https://images.pexels.com/photos/8292348/pexels-photo-8292348.jpeg',
+    'https://images.pexels.com/photos/6640619/pexels-photo-6640619.jpeg',
+    'https://images.pexels.com/photos/11744264/pexels-photo-11744264.jpeg',
+    'https://images.pexels.com/photos/7736773/pexels-photo-7736773.jpeg',
+    'https://images.pexels.com/photos/8951408/pexels-photo-8951408.jpeg',
+]
+
+
 
 # USERS
-mike1 = User.create!(
+User.create!(
     name: "mike1", 
     email: "mike1@email.com", 
     password: "123456"
 )
 
-mike2 = User.create!(
-    name: "mike2", #Faker::Name.name
-    email: "mike2@email.com", 
-    password: "123456"
-)
+19.times do# |i|
+    name = Faker::Name.unique.name
+    email = Faker::Internet.safe_email(name: name)
+    User.create!(
+        name: name,
+        email: email,
+        password: "123456"
+    )
+end
 
-mike3 = User.create!(
-    name: "mike3", 
-    email: "mike3@email.com", 
-    password: "123456"
-)
-
-mike4 = User.create!(
-    name: "mike4", 
-    email: "mike4@email.com", 
-    password: "123456"
-)
-
-demoUser = User.create!(
+User.create!(
     name: "demo user",
     email: "demo@email.com",
     password: "123456"
@@ -65,61 +118,69 @@ demoUser = User.create!(
 bigburger = Business.create!(
     name: "Big Burger",
     location: "12 street",
-    phone: 5555555,
+    phone: 555-5555,
     website: "bigburger.com",
     owner_id: 1
 )
-bigburger.photo.attach(io: b1, filename: 'b1.png')
-# bigburger.photo.attach(io: test, filename: 'https://images.pexels.com/photos/1267320/pexels-photo-1267320.jpeg')
-# bigburger.photos.attach(io: b2, filename: 'b2.jpg')
-# bigburger.photos.attach(io: b3, filename: 'b3.jpg')
-
-littleburger = Business.create!(
-    name: "Little Burger",
-    location: "2 street",
-    phone: 5555556,
-    website: "littleburger.com",
-    owner_id: 2
-)
-littleburger.photo.attach(io: b3, filename: 'b3.jpg')
-
-midburger = Business.create!(
-    name: "Mid Burger",
-    location: "6 street",
-    phone: 5555557,
-    website: "midburger.com",
-    owner_id: 3
-)
-midburger.photo.attach(io: b2, filename: 'b2.jpg')
-
-badburger = Business.create!(
-    name: "BAD Burger",
-    location: "14 BAD st.",
-    phone: 5555566,
-    website: "badburger.com",
-    owner_id: 4
-)
-
 # bigburger.photo.attach(io: b1, filename: 'b1.png')
-# midburger.photo.attach(io: b2, filename: 'b2.jpg')
-# littleburger.photo.attach(io: b3, filename: 'b3.jpg')
+_url = stockPhotoUrls.shift
+bigburger.photo.attach(io: open(_url), filename: _url)
+_url = stockPhotoUrls.shift
+bigburger.photos.attach(io: open(_url), filename: _url)
+_url = stockPhotoUrls.shift
+bigburger.photos.attach(io: open(_url), filename: _url)
+
+19.times do |i|
+    name = Faker::Restaurant.unique.name
+    location = Faker::Address.unique.full_address
+    phone = Faker::PhoneNumber.unique.cell_phone
+    website = Faker::Internet.domain_name(domain: name)
+
+    business = Business.create!(
+        name: name,
+        location: location,
+        phone: phone,
+        website: website,
+        owner_id: (i+2)
+    )
+    
+    _url = stockPhotoUrls.shift
+    business.photo.attach(io: open(_url), filename: _url)
+    _url = stockPhotoUrls.shift
+    business.photos.attach(io: open(_url), filename: _url)
+    _url = stockPhotoUrls.shift
+    business.photos.attach(io: open(_url), filename: _url)
+end
 
 #REVIEWS
-r1 = Review.create!(
-    user_id: 1,
-    business_id: 2,
-    rating: 2,
-    body: "burger is too little"
-)
-r2 = Review.create!(
-    user_id: 2,
-    business_id: 3,
-    rating: 3,
-    body: "just right"
-)
-r3 = Review.create!(
-    user_id: 3,
-    business_id: 1,
-    rating: 4,
-    body: "burger is too big"
-)
+
+20.times do |i|
+    review = Faker::Restaurant.review
+    review = Faker::Quote.unique.famous_last_words if ((i+1)%4 == 0)
+    Review.create!(
+        user_id: (i+1),
+        business_id: (20-i),
+        rating: [1,2,3,4,5].sample,
+        body: review
+    )
+end
+
+20.times do |i|
+    review = Faker::Restaurant.review
+    # review = Faker::Quote.unique.famous_last_words if ((i+1)%4 == 0)
+    Review.create!(
+        user_id: (i+1),
+        business_id: ((i+2)%20)+1,
+        rating: [1,2,3,4,5].sample,
+        body: review
+    )
+end
+
+# Business.each do |business|
+#     _url = stockPhotoUrls.shift
+#     business.photo.attach(io: open(_url), filename: _url)
+#     _url = stockPhotoUrls.shift
+#     business.photos.attach(io: open(_url), filename: _url)
+#     _url = stockPhotoUrls.shift
+#     business.photos.attach(io: open(_url), filename: _url)    
+# end
